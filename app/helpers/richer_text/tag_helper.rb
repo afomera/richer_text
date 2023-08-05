2c58618633066eb6cfca557f4a6a41b7bb755dea
Andrea Fomera <afomera@hey.com>
2023-08-05 15:11:00 -0600

    Update tag helper to reflect the ability to pass input to richer text to have it automatically update the hidden field

--- app/helpers/richer_text/tag_helper.rb
+++ app/helpers/richer_text/tag_helper.rb
@@ -4,12 +4,15 @@ module ActionView::Helpers
   class Tags::Editor < Tags::Base
     include Tags::Placeholderable
 
+    delegate :dom_id, to: ActionView::RecordIdentifier
+
     def render
       options = @options.stringify_keys
-      options["value"] = options.fetch("value") { value&.to_html }
       add_default_name_and_id(options)
+      options["input"] ||= dom_id(object, [options["id"], :richer_text_input].compact.join("_")) if object
+      options["value"] = options.fetch("value") { value&.to_html }
 
-      @template_object.richer_text_area_tag(options["name"], options["value"], options.except("value"))
+      @template_object.richer_text_area_tag(options.delete("name"), options["value"], options.except("value"))
     end
   end
 
@@ -26,24 +29,21 @@ module ActionView::Helpers
   end
 end
 
-
 module RicherText
   module TagHelper
     cattr_accessor(:id, instance_accessor: false) { 0 }
 
     def richer_text_area_tag(name, value = nil, options = {})
       options = options.symbolize_keys
-      options[:id] ||= "editor_input_#{TagHelper.id += 1}"
-      options[:class] ||= "editor-input"
+      options[:input] ||= "richer_text_input_#{RicherText::TagHelper.id += 1}"
 
       # So that we can access the content in the tiptap editor
       options[:content] ||= value
 
-      # editor_toolbar(id: options[:id]) + content_tag("textarea", value, options)
-      content_tag("div", data: { controller: "richer-text-editor", action: "editor:update->richer-text-editor#update" }) do
-        hidden_field_tag(name, value, { class: "w-full", data: { richer_text_editor_target: "input" } }) +
-        tag("richer-text-editor", options)
-      end
+      input_tag = hidden_field_tag(name, value, id: options[:input])
+      editor_tag = tag("richer-text-editor", options)
+
+      input_tag + editor_tag
     end
   end
 end
