bb6e0c5fbb00cdea6345afb422fd1c8902d4b798
Andrea Fomera <afomera@hey.com>
2023-09-19 13:21:33 -0600

    Change from json: true to 'store_as: :json' for the has_richer_text attribute

--- lib/richer_text/attribute.rb
+++ lib/richer_text/attribute.rb
@@ -3,10 +3,10 @@ module RicherText
     extend ActiveSupport::Concern
 
     class_methods do
-      def has_richer_text(name, json: false)
+      def has_richer_text(name, store_as: :html)
         # Store if the attribute is using JSON or not.
         class_attribute :"richer_text_#{name}_json", instance_writer: false
-        self.send(:"richer_text_#{name}_json=", json)
+        self.send(:"richer_text_#{name}_json=", store_as == :json)
 
         class_eval <<-CODE, __FILE__, __LINE__ + 1
           def #{name}
@@ -23,7 +23,7 @@ module RicherText
         CODE
 
         has_one :"richer_text_#{name}", -> { where(name: name) },
-          class_name: json ? "RicherText::JsonText" : "RicherText::RichText", as: :record, inverse_of: :record, autosave: true, dependent: :destroy
+          class_name: store_as == :json ? "RicherText::JsonText" : "RicherText::RichText", as: :record, inverse_of: :record, autosave: true, dependent: :destroy
 
         scope :"with_richer_text_#{name}", -> { includes("richer_text_#{name}") }
       end
