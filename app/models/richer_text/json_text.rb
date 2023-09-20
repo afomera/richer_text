702b8360865d2211fba0d7d5743767e9b58ce9c0
Andrea Fomera <afomera@hey.com>
2023-09-19 21:31:23 -0600

    Add support for attaching attachments for Rhino content so it won't be purged

--- app/models/richer_text/json_text.rb
+++ app/models/richer_text/json_text.rb
@@ -7,8 +7,9 @@ module RicherText
     serialize :body, JSON, default: DEFAULT_BODY.to_json
 
     has_many_attached :images
+    has_many_attached :rhino_attachments # For handling attachments in the Rhino Editor
 
-    before_save :update_images
+    before_save :update_attachments
 
     def to_editor_format
       body
@@ -20,16 +21,28 @@ module RicherText
 
     private
 
-    def update_images
+    def update_attachments
       self.images = image_nodes.map(&:signed_id)
+      self.rhino_attachments = rhino_attachment_records.map(&:signed_id)
     end
 
     def image_nodes
       find_nodes_of_type(RicherText::Nodes::Image).flatten.compact_blank
     end
 
+    def rhino_attachment_nodes
+      find_nodes_of_type(RicherText::Nodes::AttachmentFigure).flatten.compact_blank
+    end
+
+    def rhino_attachment_records
+      sgids = rhino_attachment_nodes.map(&:sgid).compact_blank
+      GlobalID::Locator.locate_many_signed(sgids, for: "attachable")
+    end
+
     def find_nodes_of_type(type, node = document)
       if node.children.any?
+        return node if node.is_a?(type)
+
         node.children.map { |child| find_nodes_of_type(type, child) }
       else
         node.is_a?(type) ? node : nil
